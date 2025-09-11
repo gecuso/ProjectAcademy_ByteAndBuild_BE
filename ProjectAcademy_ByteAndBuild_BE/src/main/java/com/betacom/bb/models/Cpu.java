9b73937020fd63c840be14011246ee5e97f8a107
ilrobb <roberto.bruno@edu.itspiemonte.it>
2025-09-11 10:21:57 +0200

    Correzione delle Entity

--- ProjectAcademy_ByteAndBuild_BE/src/main/java/com/betacom/bb/models/Cpu.java
+++ ProjectAcademy_ByteAndBuild_BE/src/main/java/com/betacom/bb/models/Cpu.java
@@ -1,12 +1,16 @@
 package com.betacom.bb.models;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -46,8 +50,8 @@ public class Cpu {
 			    referencedColumnName = "id")
 	private Prodotto prodotto;
 	
-	@ManyToOne
-	@JoinColumn(name = "id_pc")
-	private Pc pc;
+	@OneToMany(mappedBy = "cpu",
+			   fetch = FetchType.EAGER)
+	private  List<Pc> pc;
 	
 }
--- ProjectAcademy_ByteAndBuild_BE/src/main/java/com/betacom/bb/models/SchedaMadre.java
+++ ProjectAcademy_ByteAndBuild_BE/src/main/java/com/betacom/bb/models/SchedaMadre.java
@@ -1,12 +1,16 @@
 package com.betacom.bb.models;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -45,8 +49,8 @@ public class SchedaMadre {
 	@JoinColumn(name = "id_formato")
 	private Formato formato; //foreign key di formato (ATX, MICROATX, MINI)
 	
-	@ManyToOne
-	@JoinColumn(name = "id_pc")
-	private Pc pc;
+	@OneToMany(mappedBy = "schedaMadre",
+			   fetch = FetchType.EAGER)
+	private  List<Pc> pc;
 
 }
